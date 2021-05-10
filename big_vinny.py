176c02966a1c0795b349b272e690e31a4c66b37f
ASquanchyPenguin <tshcmiller@gmail.com>
2021-05-10 00:58:27 -0500

    Added time functions and version command

--- big_vinny.py
+++ big_vinny.py
@@ -14,7 +14,7 @@ token = config.token
 
 # Bot Information
 name = "Big Vinny"
-version = "0.2"
+version = "0.3"
 
 # Log the bot into Discord
 @client.event
@@ -26,17 +26,35 @@ async def on_ready():
 # Called when the bot receives a message
 @client.event
 async def on_message(message):
+    # Prevents the bot from responding to itself
     if message.author == client.user:
         return
         
-    # Variables to determine if bot should respond
+    # Variables to determine if and how the bot should respond
     mentioned = client.user.mentioned_in(message)
     content = message.content.lower()
+    array = content.split()
+    
+    # Check for built-in commands
+    if mentioned and any(word in content for word in botkb.commands):
+        if len(array) > 0:
+            await run_command(array[1], message.channel)
+            return
     
     # Checks if the bot should respond
     if mentioned or any(word in content for word in botkb.aliases):
         await message.channel.send("I should respond")
+        
 # end on_message()
+
+async def run_command(cmd, channel):
+    if cmd == "!time":
+        day = botkb.get_day()
+        date = botkb.get_date("%B %d, %Y")
+        time = botkb.get_time("%H:%M") 
+        await channel.send("It is {}, {} at {}.".format(day, date, time))
+    elif cmd == "!version":
+        await channel.send("Thanks for asking! I'm running version {}.".format(version))
 
 # Run the program based on the bot's token
 client.run(token)
--- botkb.py
+++ botkb.py
@@ -1,2 +1,28 @@
+from datetime import date
+from datetime import datetime
+
 # Common lists used by the bot
 aliases = ["vinny", "yelly", "chig winny", "big beep beep"]
+commands = ["!version", "!time"]
+weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+# Common tasks for the bot
+
+def get_date(format):
+    today = date.today()
+    time = today.strftime(format)
+    return time
+# end get_date()
+
+def get_day():
+    today = date.today()
+    index = today.weekday()
+    return weekdays[index]
+# end get_day()
+
+def get_time(format):
+    today = datetime.now()
+    time = today.strftime(format)
+    return time
+# end get_time()
+
